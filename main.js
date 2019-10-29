1fe9511adcbb1f775cb1f79a3ea1b72a534e534d
Loki <lochlan@appswiz.com>
2019-10-29 19:33:51 +1100

    populating bootstrap card components with api data

--- main.js
+++ main.js
@@ -16,6 +16,21 @@ const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 axios.get(url)
 .then(function (response) {
     let repos = response.data;
+
+    // for of loop here which will create & populate a new card for each repo.
+    // will look like this...
+    for (repo of repos) {
+        let cardHTML = `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${repo.name}</h5>
+          <p class="card-text">${repo.description}</p>
+        </div>
+      </div>`;
+      //let card = document.createElement('div');
+      //card.innerHTML = cardHTML;
+      //document.body.appendChild(card);
+      document.body.innerHTML += cardHTML;
+    };
     console.log(response.data);
 })
 .catch(function (error) {
